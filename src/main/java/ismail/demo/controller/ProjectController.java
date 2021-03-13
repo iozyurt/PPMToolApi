cea562d8d0fd8f6b9842d973129be3c5e5334ad5
iozyurt <ismozy09@gmail.com>
2021-03-13 13:38:49 +0300

    Add remaining crud operations in Project service and controller

--- src/main/java/ismail/demo/controller/ProjectController.java
+++ src/main/java/ismail/demo/controller/ProjectController.java
@@ -7,10 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -35,5 +32,38 @@ public class ProjectController {
 
         projectService.saveOrUpdateProject(null, project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
+    }
+
+    @GetMapping("{projectId}")
+    public ResponseEntity<?> getProjectById(@PathVariable String projectId) {
+
+        Project project = projectService.findProjectByIdentifier(projectId);
+
+        return new ResponseEntity<Project>(project, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "")
+    public Iterable<Project> getAllProjects() {
+        return projectService.findAllProjects();
+    }
+
+    @DeleteMapping("{projectId}")
+    public ResponseEntity<?> deleteProject(@PathVariable String projectId) {
+
+        Project project = projectService.findProjectByIdentifier(projectId);
+        projectService.deleteProjectByIdentifier(projectId);
+        return new ResponseEntity<String>("Project with ID: '" + projectId + "' was deleted", HttpStatus.OK);
+    }
+
+    @PutMapping("{projectId}")
+    public ResponseEntity<?> updateProject(@PathVariable Long projectId, @Valid @RequestBody Project project, BindingResult result) {
+
+
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+        if (errorMap != null) return errorMap;
+
+        projectService.saveOrUpdateProject(projectId, project);
+
+        return new ResponseEntity<Project>(project, HttpStatus.OK);
     }
 }
--- src/main/java/ismail/demo/services/ProjectService.java
+++ src/main/java/ismail/demo/services/ProjectService.java
@@ -4,7 +4,6 @@ import ismail.demo.exceptions.ProjectIdException;
 import ismail.demo.exceptions.ProjectNotFoundException;
 import ismail.demo.model.Project;
 import ismail.demo.repository.ProjectRepository;
-import org.hibernate.Session;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -21,7 +20,7 @@ public class ProjectService {
         //Logic
         if (projectId != null) {
             Project projectOptional = projectRepository.findByProjectIdentifier(project.getProjectIdentifier());
-            if (projectId.equals( project.getId())) throw new ProjectIdException( "Project ID 's not the same");
+            if (projectId.equals(project.getId())) throw new ProjectIdException("Project ID 's not the same");
             else if (projectOptional == null)
                 throw new ProjectNotFoundException("Project ID: '" + projectOptional.getProjectIdentifier() + "' does not exists");
 
@@ -37,7 +36,29 @@ public class ProjectService {
         } catch (Exception e) {
             throw new ProjectIdException("Project ID: '" + project.getProjectIdentifier().toUpperCase() + "' already exists. It has to be Unique.");
         }
+    }
 
+    public Project findProjectByIdentifier(String projectId) {
 
+        Project project = projectRepository.findByProjectIdentifier(projectId);
+
+        if (project == null) {
+            throw new ProjectNotFoundException("Project ID: '" + projectId + "' does not exists");
+        }
+
+        return project;
+    }
+
+    public Iterable<Project> findAllProjects() {
+        return projectRepository.findAll();
+    }
+
+    public void deleteProjectByIdentifier(String projectId) {
+        Project project = findProjectByIdentifier(projectId.toUpperCase());
+        if (project == null) {
+            throw new ProjectNotFoundException("Project ID: '" + projectId + "' does not exists");
+        }
+
+        projectRepository.delete(project);
     }
 }
